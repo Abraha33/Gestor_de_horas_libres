36aefe7a65a6addf9ad98642bb89af70ccab6f1b
Abraha33 <168048145+Abraha33@users.noreply.github.com>
2024-05-15 18:09:36 -0500

    Update core.py

--- core.py
+++ core.py
@@ -1,5 +1,4 @@
 import json
-import datetime as dt 
 
 def crearInfo(*args):
     if(checkFile(args[0])==False):
@@ -43,20 +42,6 @@ def LoadInfo(fileName):
             dicc = new_func(read_file)
             read_file.close()
         return dicc
-
-def new_func(read_file):
-    large=dt.dt(frriendyback:check_file('read_file'))
-    dicc = json.load(read_file)
-    return dicc
-def erraser_backup(read_file):
-    remote.dicc=json.load(read_file)
-    if dicc is large and dicc is not num:
-        print('the number is full and is out of range')
-    else:
-        brunch_back=large+dicc{read_file}
-        print(success, 'retual prompt')
-    return dicc as d
-    
 
 def checkFile(filePath):
     try:
